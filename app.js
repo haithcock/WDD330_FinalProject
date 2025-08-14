c075381deec138c694c868974bae79775d025ed9
haithcock <dakota.haith1997@.com>
2025-08-14 02:09:25 -0400

    updoots

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'css')));
 
 
 app.use(express.json());
